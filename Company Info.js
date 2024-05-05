6d198a8f510644749a99571020488b6f6c3c1be6
Suleiman Shakhtour <suleiman.shakhtour@gmail.com>
2024-05-05 12:46:01 +0300

    Update Company Info.js

--- Company Info.js
+++ Company Info.js
@@ -1,66 +1,81 @@
-function cancel(){
+function cancel() {
     $("button.form-close").click();
 }
 
-$(function(){
+$(function () {
 
-$('iframe').load(function() {
-$('iframe.quickform').contents().find('.entity-form').each(function(){ $(this).css('border','none');  });
-$('iframe.quickform').contents().find('.form-control-cell').each(function(){$(this).css('padding-right','0px'); $(this).css('margin','0px'); $(this).css('background-color','#FFFFFF');});
+    $('iframe').load(function () {
+        $('iframe.quickform').contents().find('.entity-form').each(function () { $(this).css('border', 'none'); });
+        $('iframe.quickform').contents().find('.form-control-cell').each(function () { $(this).css('padding-right', '0px'); $(this).css('margin', '0px'); $(this).css('background-color', '#FFFFFF'); });
 
-})    
-$('.crmquickform-cell').css('border','none');
+    })
+    $('.crmquickform-cell').css('border', 'none');
 
-$('#quickViewForm_CompanyAddress').load(function() {
-  $('#quickViewForm_CompanyAddress').contents().find('#icv_companyaddress').closest('td').css('padding-right','0px')
+    $('#quickViewForm_CompanyAddress').load(function () {
+        $('#quickViewForm_CompanyAddress').contents().find('#icv_companyaddress').closest('td').css('padding-right', '0px')
+    })
+
+
+    $('#PlanContacts section.modal.modal-form.modal-form-insert').addClass('company-info-modal sc_modal')
+    $('#PlanBusinessDescription section.modal.modal-form.modal-form-insert').addClass('business-desc-modal sc_modal')
+    $('#PlanCompanyOwnership section.modal.modal-form.modal-form-insert').addClass('company-own-modal sc_modal')
+    $('#PlanContacts section.modal.modal-form.modal-form-edit').addClass('company-info-modal sc_modal')
+    $('#PlanBusinessDescription section.modal.modal-form.modal-form-edit').addClass('business-desc-modal sc_modal')
+    $('#PlanCompanyOwnership section.modal.modal-form.modal-form-edit').addClass('company-own-modal sc_modal')
+    $('#PlanContacts section.modal.modal-form.modal-form-details').addClass('company-info-modal sc_modal')
+    $('#PlanBusinessDescription section.modal.modal-form.modal-form-details').addClass('business-readonly-modal sc_modal')
+    $('#PlanCompanyOwnership section.modal.modal-form.modal-form-details').addClass('company-own-modal sc_modal')
+
+
+    $('.modal-title .fa.fa-pencil-square-o').css('display', 'none');
+    $('.modal-title .fa.fa-info-circle').css('display', 'none');
+    $('.modal-form-insert.company-info-modal .modal-title').text('Add Additional Contact');
+    $('.modal-form-insert.business-desc-modal .modal-title').text('Add New Main Activity');
+    $('.modal-form-insert.company-own-modal .modal-title').text('Add New Company Ownership');
+    $('.modal-form-edit.company-info-modal .modal-title').text('Edit Additional Contact');
+    $('.modal-form-edit.business-desc-modal .modal-title').text('Edit Main Activity');
+    $('.modal-form-edit.company-own-modal .modal-title').text('Edit Company Ownership');
+    $('.modal-header .form-close').css('display', 'none');
+
+
+    $('.modal-title').removeAttr('title');
+    $('div.actions').addClass('form-custom-actions') // add custom class to actions div 
+    $(".form-custom-actions").append('<div id="div-action-btns"><button type="button"  id="btn-next"  class="btn btn-primary button">Next</button></div>');
+    $('.form-custom-actions').addClass('card-div');
+
+
+    $('button#btn-next').click(function () {
+        // Define the trusted domain
+        var trustedDomain = '{{ sitemarkers["PlanContractInfo"].url }}';
+
+        // Get the redirection URL
+        var redirectUrl = trustedDomain + window.location.search;
+
+        // Perform client-side validation
+        if (isValidRedirect(redirectUrl, trustedDomain)) {
+            window.location.href = redirectUrl;
+        } else {
+            // Handle the redirection to an untrusted site
+            // For example, redirect to a default or error page
+            window.location.href = 'https://tawteenicvdevelopment.powerappsportals.com/'; // Replace with your default page URL
+        }
+    });
+
+    $("#BusinessDescription .entity-grid.subgrid").on("loaded", function () {
+        if ($('#BusinessDescription .entity-grid.subgrid table tbody tr').length == 3) {
+            $('#btn-newBusiness').addClass('btn-disabled');
+        }
+        else {
+            $('#btn-newBusiness').removeClass('btn-disabled');
+        }
+    })
+
+
+
 })
 
-
-$('#PlanContacts section.modal.modal-form.modal-form-insert').addClass('company-info-modal sc_modal') 
-$('#PlanBusinessDescription section.modal.modal-form.modal-form-insert').addClass('business-desc-modal sc_modal') 
-$('#PlanCompanyOwnership section.modal.modal-form.modal-form-insert').addClass('company-own-modal sc_modal') 
-$('#PlanContacts section.modal.modal-form.modal-form-edit').addClass('company-info-modal sc_modal') 
-$('#PlanBusinessDescription section.modal.modal-form.modal-form-edit').addClass('business-desc-modal sc_modal') 
-$('#PlanCompanyOwnership section.modal.modal-form.modal-form-edit').addClass('company-own-modal sc_modal') 
-$('#PlanContacts section.modal.modal-form.modal-form-details').addClass('company-info-modal sc_modal') 
-$('#PlanBusinessDescription section.modal.modal-form.modal-form-details').addClass('business-readonly-modal sc_modal') 
-$('#PlanCompanyOwnership section.modal.modal-form.modal-form-details').addClass('company-own-modal sc_modal') 
-
-
-$('.modal-title .fa.fa-pencil-square-o').css('display','none');
-$('.modal-title .fa.fa-info-circle').css('display','none');
-$('.modal-form-insert.company-info-modal .modal-title').text('Add Additional Contact');
-$('.modal-form-insert.business-desc-modal .modal-title').text('Add New Main Activity');
-$('.modal-form-insert.company-own-modal .modal-title').text('Add New Company Ownership');
-$('.modal-form-edit.company-info-modal .modal-title').text('Edit Additional Contact');
-$('.modal-form-edit.business-desc-modal .modal-title').text('Edit Main Activity');
-$('.modal-form-edit.company-own-modal .modal-title').text('Edit Company Ownership');
-$('.modal-header .form-close').css('display','none');
-
-
-$('.modal-title').removeAttr('title');
-$('div.actions').addClass('form-custom-actions') // add custom class to actions div 
-$(".form-custom-actions").append('<div id="div-action-btns"><button type="button"  id="btn-next"  class="btn btn-primary button">Next</button></div>');
-$('.form-custom-actions').addClass('card-div');
-
-
-$('button#btn-next').click(function(){     
-    window.location.href =  '{{ sitemarkers["PlanContractInfo"].url }}' + window.location.search;
- 
-})
-
-
-$("#BusinessDescription .entity-grid.subgrid").on("loaded", function(){ 
-    if($('#BusinessDescription .entity-grid.subgrid table tbody tr').length == 3)
-    {
-        $('#btn-newBusiness').addClass('btn-disabled');
-    }
-    else 
-    {
-         $('#btn-newBusiness').removeClass('btn-disabled');
-    }
-})
-
-
-
-})
+function isValidRedirect(redirectUrl, trustedDomain) {
+    // Check if the redirectUrl starts with the trustedDomain
+    // You can customize this validation logic based on your requirements
+    return redirectUrl.startsWith(trustedDomain);
+}
